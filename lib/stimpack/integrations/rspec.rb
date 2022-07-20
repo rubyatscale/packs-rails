1b84eeb196d786fd965c01a6d876f3c08dad5799
Josh Becker <beckerbi@gmail.com>
2022-07-20 09:22:54 -0400

    Check if ::RSpec has a method named configuration

--- lib/stimpack/integrations/rspec.rb
+++ lib/stimpack/integrations/rspec.rb
@@ -3,6 +3,7 @@ module Stimpack
     class RSpec
       def self.install(app)
         return unless defined?(::RSpec)
+        return unless ::RSpec.respond_to?(:configuration)
 
         Packs.each do |pack|
           ::RSpec.configuration.pattern.concat(",#{pack.relative_path.join("spec/**/*_spec.rb")}")
