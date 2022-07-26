164f95357b99f028ab61818a93da285c36362420
Ngan Pham <ngan@users.noreply.github.com>
2022-07-26 11:47:09 -0700

    Add comments

--- lib/stimpack/integrations/rspec.rb
+++ lib/stimpack/integrations/rspec.rb
@@ -3,6 +3,12 @@ module Stimpack
     class RSpec
       def self.install(app)
         return unless defined?(::RSpec)
+        # Sometimes, the `rspec-rails` gem is installed in the development
+        # group. This means the ::RSpec module will be defined. However, this
+        # doesn't mean that we've loaded rspec-core, or even about to run tests.
+        # Let's make sure we are actually loading the test environment before
+        # installing our integration. An easy of doing this is seeing of the
+        # configuration method exists on RSpec.
         return unless ::RSpec.respond_to?(:configuration)
 
         Packs.each do |pack|
