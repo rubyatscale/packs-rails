6251e9dcfa733647239f12c9b14be96969a440dc
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-08-12 14:54:01 -0400

    add clarifying comment

--- lib/stimpack/railtie.rb
+++ lib/stimpack/railtie.rb
@@ -4,6 +4,8 @@ module Stimpack
   class Railtie < Rails::Railtie
     config.before_configuration do |app|
       Stimpack.load(app)
+      # This is not used within stimpack. Rather, this allows OTHER tools to
+      # hook into Stimpack via ActiveSupport hooks.
       ActiveSupport.run_load_hooks(:stimpack, Packs)
     end
   end
