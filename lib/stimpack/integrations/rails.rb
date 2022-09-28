20e29037e0368b1a8a3e2b836eeaaef044919aa9
Gary Passero <gpassero@gmail.com>
2022-09-28 09:52:27 -0400

    Make it run

--- lib/stimpack/integrations/rails.rb
+++ lib/stimpack/integrations/rails.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 require "active_support/inflections"
+require 'pry'
 
 module Stimpack
   module Integrations
     class Rails
       def initialize(app)
         @app = app
-
+        ::Rails.autoloaders.main.log!
         Stimpack.config.paths.freeze
         create_engines
         inject_paths
@@ -23,8 +24,11 @@ module Stimpack
 
       def inject_paths
         Packs.all.each do |pack|
+          module_name = pack.name.camelize
+          Object.const_set(module_name, Module.new) if pack.config.automatic_pack_namespace? #
           Stimpack.config.paths.each do |path|
-            @app.paths[path] << pack.path.join(path)
+            @app.paths[path] << pack.path.join(path) unless pack.config.automatic_pack_namespace?
+            ::Rails.autoloaders.main.push_dir(pack.path.join(path), namespace: module_name.constantize) if pack.config.automatic_pack_namespace? && Dir.exist?(pack.path.join(path))
           end
         end
       end
