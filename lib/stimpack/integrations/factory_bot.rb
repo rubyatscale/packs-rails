982f297c1013cdb0a40715fe939a6e398c533a07
Alex Evanczuk <alex.evanczuk@gusto.com>
2022-12-29 09:28:05 -0500

    Allow packs configuration to be set in one place

--- lib/stimpack/integrations/factory_bot.rb
+++ lib/stimpack/integrations/factory_bot.rb
@@ -5,6 +5,7 @@ module Stimpack
     class FactoryBot
       def initialize(app)
         return unless app.config.respond_to?(:factory_bot)
+        Stimpack.configure_packs
 
         Packs.all.each do |pack|
           app.config.factory_bot.definition_file_paths << pack.relative_path.join("spec/factories").to_s
