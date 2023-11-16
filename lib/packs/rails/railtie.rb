37e71cab963a5b6aca6ebea491657f14e7b2fb67
matias <mecheverria@buk.cl>
2023-11-16 18:02:39 -0300

    fix: factory_bot integration

--- lib/packs/rails/railtie.rb
+++ lib/packs/rails/railtie.rb
@@ -5,11 +5,14 @@ module Packs
     class Railtie < ::Rails::Railtie
       config.before_configuration do |app|
         Integrations::Rails.new(app)
-        Integrations::FactoryBot.new(app)
-
+        
         # This is not used within packs-rails. Rather, this allows OTHER tools to
         # hook into packs-rails via ActiveSupport hooks.
         ActiveSupport.run_load_hooks(:packs_rails, Packs)
+      end
+      
+      config.after_initialize do |app|
+        Integrations::FactoryBot.new(app)
       end
     end
   end
