d0191dd8ae72cc1fcf2052d205efb23fe324cbb9
Todd Sedano <todd.sedano@gusto.com>
2024-02-02 14:03:48 -0800

    Add spring watch
    When a pack moves, we want spring to update the loaded classes
    so that packwerk prints correct results
    
    Co-authored-by: Teal Stannard <teal.stannard@gusto.com>
    Co-authored-by: Ashley Willard <ashley.willard@gusto.com>
    Co-authored-by: Joey Schoblaska <joey.schoblaska@gusto.com>

--- lib/packs/rails/railtie.rb
+++ lib/packs/rails/railtie.rb
@@ -11,7 +11,19 @@ module Packs
         # hook into packs-rails via ActiveSupport hooks.
         ActiveSupport.run_load_hooks(:packs_rails, Packs)
       end
-      
+
+      config.after_initialize do |app|
+        if defined?(Spring)
+          puts "Hello Db Blasters!"
+          Packs::Specification::Configuration.fetch.pack_paths.each do |dir|
+            puts "#{dir}"
+            Dir["#{dir}/package.yml"].each { |f|
+              puts f
+              Spring.watch(f)
+            }
+          end
+        end
+      end
     end
   end
 end
