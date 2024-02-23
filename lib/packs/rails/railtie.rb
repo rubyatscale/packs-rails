99e9669f449388e35f2c6244f1a650ed92cd15f9
Perry Hertler <perry.hertler@gusto.com>
2024-02-23 15:45:25 -0600

    Configuring spring.watch in railtie before_configuration
    Co-authored-by: Ashley Willard <ashley.willard@gusto.com>
    Co-authored-by: Joey Schoblaska <joey.schoblaska@gusto.com>

--- lib/packs/rails/railtie.rb
+++ lib/packs/rails/railtie.rb
@@ -10,9 +10,7 @@ module Packs
         # This is not used within packs-rails. Rather, this allows OTHER tools to
         # hook into packs-rails via ActiveSupport hooks.
         ActiveSupport.run_load_hooks(:packs_rails, Packs)
-      end
 
-      config.after_initialize do |app|
         if defined?(Spring)
           Packs::Specification::Configuration.fetch.pack_paths.each do |dir|
             Dir["#{dir}/package.yml"].each do |package_yml|
